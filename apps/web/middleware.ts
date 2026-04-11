f31bd6ab5f65a73959c592e0e5b38360d824d9bd
Daan Balm <daanmlab@gmail.com>
2026-04-11 08:59:04 +0200

    feat: update middleware matcher to exclude API routes from processing

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -16,5 +16,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.svg).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|api|.*\\.svg).*)'],
 };
